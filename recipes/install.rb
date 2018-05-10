1920d421fa442d85178fabe75147abfea9a1fa02
ohr486 <ohr486@gmail.com>
2018-05-10 21:25:24 +0900

    add travis base settings

---
+++ recipes/install.rb
@@ -0,0 +1,6 @@
+case node[:platform]
+when "debian", "ubuntu"
+  execute "apt-get update"
+end
+
+include_recipe "grafana"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,22 @@
 require "bundler/setup"
 require "itamae/plugin/recipe/grafana"
 
+require "yaml"
+require "itamae/node"
+
+require "serverspec"
+require "docker"
+
+set :backend, :docker
+set :docker_image, ENV["DOCKER_IMAGE"]
+set :docker_container, ENV["DOCKER_CONTAINER"]
+
+def node
+  return @node if @node
+  hash = YAML.load_file("#{__dir__}/../recipes/node.yml")
+  @node = Itamae::Node.new(hash, Specinfra.backend)
+end
+
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
